f83175fa2735a160bade4f1f5788492826e4a540
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-06-23 11:47:49 +0100

    animate Character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,12 +4,19 @@ class MovableObject {
     width = 180;
     height = 200;
     img;
-
-    
+    imageCache = [];
 
     loadImage(path){
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr){
+        arr.forEach((path)=>{
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
