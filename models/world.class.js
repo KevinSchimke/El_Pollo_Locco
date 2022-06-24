87673aa0b3cb32e0e07e8361b39cfd36de9bc994
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-06-24 02:03:48 +0100

    include Audio files

--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,6 @@
 class World{
     character = new Character();
-    enemies = level1.enemies;
-    clouds = level1.clouds;
-    backgroundObjects = level1.backgroundObjects;
+    level = level1;
     canvas;
     ctx;
     keyboard;
@@ -27,10 +25,11 @@ class World{
 
         this.ctx.translate(this.camera_x,0);
 
-        this.addObjectToMap(this.backgroundObjects);
+        this.addObjectToMap(this.level.backgroundObjects);
+
         this.addToMap(this.character);
-        this.addObjectToMap(this.enemies);
-        this.addObjectToMap(this.clouds);
+        this.addObjectToMap(this.level.enemies);
+        this.addObjectToMap(this.level.clouds);
 
         this.ctx.translate(-this.camera_x,0);
 
