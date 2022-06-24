723858b4bcfdf16900dec0ffea5e0b4f685b7a13
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-06-24 02:42:12 +0100

    include Endboss

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -7,6 +7,8 @@ class Chicken extends MovableObject{
         './img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         './img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
+    chicken_sound = new Audio('./audio/chicken.mp3');
+    chicken_souund_interval = 3000;
 
     constructor(){
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
@@ -20,11 +22,11 @@ class Chicken extends MovableObject{
         this.moveLeft();
 
         setInterval(() => {
-            let i =  this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;  
-        },250)
+            this.playAnimation(this.IMAGES_WALKING);
+        },250);
+        setInterval(() => {
+            this.chicken_sound.play();
+        },this.chicken_souund_interval);
     }
 
 }
---
+++ models/endboss.class.js
@@ -0,0 +1,27 @@
+class Endboss extends MovableObject{
+
+    height = 200;
+    width = 200;
+    y = 240;
+
+
+    IMAGES_WALKING =[
+        './img/4_enemie_boss_chicken/1_walk/G1.png',
+        './img/4_enemie_boss_chicken/1_walk/G2.png',
+        './img/4_enemie_boss_chicken/1_walk/G3.png',
+        './img/4_enemie_boss_chicken/1_walk/G4.png'
+    ];
+
+    constructor(){
+        super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 350;
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        },250);
+    }
+}
