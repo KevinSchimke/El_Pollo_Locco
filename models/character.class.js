9c4dd1c6c7020547cb5b320f9b60d650027ca3cf
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-07-15 20:06:48 +0100

    jump animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,6 @@
-class Character extends MovableObject{
+class Character extends MovableObject {
 
-    IMAGES_WALKING =[
+    IMAGES_WALKING = [
         './img/2_character_pepe/2_walk//W-21.png',
         './img/2_character_pepe/2_walk//W-22.png',
         './img/2_character_pepe/2_walk//W-23.png',
@@ -8,7 +8,7 @@ class Character extends MovableObject{
         './img/2_character_pepe/2_walk//W-25.png',
         './img/2_character_pepe/2_walk//W-26.png'
     ];
-    IMAGES_JUMPING =[
+    IMAGES_JUMPING = [
         './img/2_character_pepe/3_jump//J-31.png',
         './img/2_character_pepe/3_jump//J-32.png',
         './img/2_character_pepe/3_jump//J-33.png',
@@ -19,16 +19,17 @@ class Character extends MovableObject{
         './img/2_character_pepe/3_jump//J-38.png',
         './img/2_character_pepe/3_jump//J-39.png'
     ];
-    
+
     y = 50;
     world;
     speed = 10;
 
     walking_sound = new Audio('./audio/running.mp3');
 
-    constructor(){
+    constructor() {
         super().loadImage('./img/2_character_pepe/1_idle/idle/I-1.png')
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.animate();
     }
@@ -37,32 +38,38 @@ class Character extends MovableObject{
 
         setInterval(() => {
             this.walking_sound.pause();
-            if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
                 this.walking_sound.play();
             }
-            if(this.world.keyboard.LEFT && this.x > 0){
+            if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
                 this.walking_sound.play();
             }
-            this.world.camera_x = - this.x +50;
-
-        },1000/60);
+            if (this.world.keyboard.UP){
+                this.speedY = 20;
+            }
+            this.world.camera_x = - this.x + 50;
+        }, 1000 / 60);
 
         setInterval(() => {
-
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
-                this.playAnimation(this.IMAGES_WALKING);
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
             }
-            },50);
-            
 
-            
+        }, 150);
+
+
+
     }
 
-    jump(){
+    jump() {
 
     }
 }
